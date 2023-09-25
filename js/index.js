5eae3f1ce8e4bcde537e578d9197671d6e3badb1
MonkeyGG2 <spinosaurus882@gmail.com>
2023-09-25 16:12:50 -0400

    removed annoying alerts

--- js/index.js
+++ js/index.js
@@ -302,21 +302,20 @@ function makecloak(replaceUrl = preferences.cloakUrl) {
         var url = window.location.href;
         const win = window.open();
         if (!win || win.closed || typeof win.closed == 'undefined') {
-            alert('Please allow Pop-Ups - By disabling popups, you allow this website to show up in your history.');
-        } else {
-            win.document.body.style.margin = "0";
-            win.document.body.style.height = "100vh";
-            var iframe = win.document.createElement("iframe");
-            iframe.style.border = "none";
-            iframe.style.width = "100%";
-            iframe.style.height = "100%";
-            iframe.style.margin = "0";
-            iframe.referrerpolicy = "no-referrer";
-            iframe.allow = "fullscreen";
-            iframe.src = url.toString();
-            win.document.body.appendChild(iframe);
-            window.location.replace(replaceUrl)
+            return;
         }
+        win.document.body.style.margin = "0";
+        win.document.body.style.height = "100vh";
+        var iframe = win.document.createElement("iframe");
+        iframe.style.border = "none";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.margin = "0";
+        iframe.referrerpolicy = "no-referrer";
+        iframe.allow = "fullscreen";
+        iframe.src = url.toString();
+        win.document.body.appendChild(iframe);
+        window.location.replace(replaceUrl)
     }
 }
 
